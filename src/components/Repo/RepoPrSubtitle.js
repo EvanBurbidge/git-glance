25a474280d8e8ae4bd80aacf8d4a1d87c999771a
Evan.Burbidge <burbidge@tcd.ie>
2021-10-12 21:09:33 +0100

    update to have data closer to icions

--- src/components/Repo/RepoPrSubtitle.js
+++ src/components/Repo/RepoPrSubtitle.js
@@ -14,9 +14,9 @@ export const RepoPrSubtitle = ({
       <div className="flex flex-col">
         <div className="flex items-center mb-2">
           <ClockIcon data-tip={`Opened ${dayjs(createdAt).format('DD/MM/YYYY')}`} className="w-4 h-4 text-primary" />
-          <p className="ml-2 mr-2 text-sm text-primary">{dayjs(createdAt).format('DD/MM/YYYY')}</p>
+          <p className="ml-1 mr-2 text-sm text-primary">{dayjs(createdAt).format('DD/MM/YYYY')}</p>
           <UserIcon className="w-4 h-4 text-primary"/>
-          <p className="ml-2 mr-2 text-sm text-primary">{author.login}</p>
+          <p className="ml-1 mr-2 text-sm text-primary">{author.login}</p>
           {commentsCount > 0 && (
             <>
               <AnnotationIcon data-tip={`${commentsCount} ${commentsCount > 1 ? 'comments' : 'comment'}`} className="w-4 h-4 text-primary" />
