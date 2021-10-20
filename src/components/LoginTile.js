fb5fb19b2b0a9f1cc157950e4dfaa3f62b47ad2d
eburbidge <eburbidge@hubspot.com>
2021-10-20 15:06:42 +0100

    add login tiles

--- src/components/LoginTile.js
+++ src/components/LoginTile.js
@@ -4,8 +4,8 @@ export const LoginTile = ({
   provider = 'github',
   imgSrc = ""
 }) => (
-  <div className="w-full border border-gray-100 rounded-sm p-4 flex align-center items-center justify-between hover:border-highlight cursor-pointer hover:outline-highlight">
+  <div className="w-full border border-primary mb-3 rounded-sm p-4 flex align-center items-center justify-between hover:border-highlight cursor-pointer hover:outline-highlight">
     <img src={imgSrc} alt="login tile icon" className="w-1/3"/>
-    <p className="text-primary font-semibold hover:text-highlight cursor:pointer">{provider}</p>
+    <p className="capitalize text-primary font-semibold hover:text-highlight cursor:pointer">{provider}</p>
   </div>
 )
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,6 +4,8 @@ import { useRouter } from '../hooks/useRouter';
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
 import Octo from '../assets/octocat.svg';
+import Git from '../assets/github.png';
+import Gitlab from '../assets/gitlab.png';
 import { LoginTile } from '../components/LoginTile';
 
 
@@ -20,13 +22,15 @@ const Login = () => {
   return (
     <>
       <Header showDropdown={false}/>
-      <div className="flex flex-col items-center justify-center w-full h-screen px-5 pt-10 text-center align-center text-primary">
-        <LoginTile provider="github.com" imgSrc={Octo}/>
-        <img src={Octo} alt="github-logo" className="w-1/2 h-auto mb-6" />
-        <h3 className="text-xl font-bold tracking-tight">
+      <div className="flex flex-col items-center justify-start pt-20 w-full h-screen px-5 text-center text-primary">
+        {/* <img src={Octo} alt="github-logo" className="w-1/2 h-auto mb-6" /> */}
+        <h3 className="text-3xl text-primary font-bold tracking-tight">
           Git Glance
         </h3>
         <p className="mt-3 mb-6 text-base text-gray-500">A quick way to glance at your repos and pull requests for easier navigation</p>
+        <LoginTile provider="github" imgSrc={Octo}/>
+        <LoginTile provider="github enterprise" imgSrc={Git}/>
+        <LoginTile provider="gitlab" imgSrc={Gitlab}/>
         <Button onClick={login}>
           Login
         </Button>
