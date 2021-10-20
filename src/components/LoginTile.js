d85ddd03e339d8226b024874e1fe9045c12741b1
eburbidge <eburbidge@hubspot.com>
2021-10-20 14:50:35 +0100

    start working on tiles

--- src/components/LoginTile.js
+++ src/components/LoginTile.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export const LoginTile = ({
   provider = 'github',
+  imgSrc = ""
 }) => (
-  <div className="w-full border border-gray-100 rounded-sm">
-    <p>{provider}</p>
+  <div className="w-full border border-gray-100 rounded-sm p-4 flex align-center items-center justify-between hover:border-highlight cursor-pointer hover:outline-highlight">
+    <img src={imgSrc} alt="login tile icon" className="w-1/3"/>
+    <p className="text-primary font-semibold hover:text-highlight cursor:pointer">{provider}</p>
   </div>
 )
